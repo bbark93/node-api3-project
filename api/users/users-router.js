4b618bb006acf549ce06b1f1c653f86d6969fa9d
Brandon Barksdale <bbark93@gmail.com>
2024-06-08 11:55:06 -0400

    get users request

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -8,10 +8,18 @@ const {
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
@@ -53,6 +61,14 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   console.log(req.user);
   console.log(req.text);
 });
+
+router .use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: 'somthing tragic inside posts router happened',
+    message: err.message,
+    stack: err.stack
+  })
+})
 
 // do not forget to export the router
 module.exports = router;
